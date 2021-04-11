f2d313c5457b2924f31ed35e00cc216b1d3654d8
earagad <reaton7@gmail.com>
2021-04-11 08:19:37 -0400

    chimp 2

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 // const request = require("request");
 const https = require("https");
-
-
-const app = express();
 
 
 app.use(express.static("public"));
