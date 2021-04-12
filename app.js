32b61f173f771646c707b372d6d7540cf6a74d39
earagad <reaton7@gmail.com>
2021-04-12 08:28:22 -0400

    #

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const bodyParser = require("body-parser");
 // const request = require("request");
 const https = require("https");
 
-app.use(express.static("public"));
-app.use(express.static("CSS"));
-app.use(express.static("Images"));
+// app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
